f3ce9ed05f7d7c6ff447cd37195cfc144c84188e
mcharamrm <mcharanrm>
2020-04-05 23:07:07 +0530

    removed un-ne if condi

--- math/polar_coordinates.py
+++ math/polar_coordinates.py
@@ -11,10 +11,8 @@ from math import acos,sqrt
 if __name__=='__main__':
     c=eval(input()) 		#complex number
     r=(sqrt(pow(c.real,2)+pow(c.imag,2)))	#radius=distance b/w origin and location of complex number in polar co-orinate system
-    i=acos(c.real/r)		#to find angle. sin(t)=a/c, cos(t)=b/c and tan(c)=a/b; height=a;base=b;hypotenuse=c ;see angle-problem
-    if str(c.real)[0]=='-' and str(c.imag)[0]=='-':	#clock-wise = -ve
-        print('{:0.3f}\n{:0.3f}'.format(r,(-i)))
-    elif str(c.imag)[0]=='-':
+    i=acos(c.real/r) 	#atan(b/a) #to find angle. sin(t)=a/c, cos(t)=b/c and tan(t)=a/b; height=a;base=b;hypotenuse=c ;see angle-problem
+    if str(c.imag)[0]=='-':
         print('{:0.3f}\n{:0.3f}'.format(r,(-i)))	#clock-wise = -ve
     else:
         print('{:0.3f}\n{:0.3f}'.format(r,i))		#counter-clock = +ve
